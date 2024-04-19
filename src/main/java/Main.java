c6cd8a263d3c2c4113f04af9c5d036a5e9d07949
ngc20 <ngc20@ngc>
2024-04-19 11:53:37 +0800

    Complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
 import Frontend.InterfaceManager;
 
 public class Main {
+    static {
+        // 在静态初始化块中加载库文件
+        System.load("/path/to/library/libawt_xawt.so");
+    }
     public static void main(String[] args) {
         InterfaceManager interfaceManager = new InterfaceManager();
         interfaceManager.display();
